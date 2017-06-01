8658ca752ed13a1f1045ebb91b6d9541ca342715
Michael Hebelka <michael.hebelka@sharpspring.com>
2017-06-01 12:41:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Golang Configuration
+====================
+
+This package just makes it simpler to deal with configuration. Just define
+a struct with the appropriate tags, and either embed the config.Config struct
+type or manually implement the interface (I recommend the former), and then
+you're essentially good to go. Observe:
+
+```go
+import (
+    "github.com/santiclause/goconfig"
+)
+type Config struct {
+	HttpPort            int           `yaml:"http_port" env:"HTTP_PORT"`
+	ConnTimeout         time.Duration `yaml:"conn_timeout" env:"CONN_TIMEOUT" required:"true"`
+	RequestGracePeriod  time.Duration `yaml:"request_grace_period" env:"REQUEST_GRACE_PERIOD"`
+	goconfig.Config
+}
+
+func main() {
+    // Make the struct, with a default value (since Load will overwrite it)
+    config := &Config{
+        HttpPort: 8080,
+    }
+    config.SetFilename("whatever.yaml")
+    goconfig.Load(&config)
+    // Optionally make it listen for SIGHUPs
+    goconfig.ListenForSignals(&config)
+}
+```
+
+
+Supported tags
+--------------
+
+* `env`: see https://github.com/caarlos0/env
+* `yaml`: see https://github.com/go-yaml/yaml
+* `required`: if this has a value of "true", Load will return an error if that
+struct field has a zero value after parsing the yaml and environment variables.
+
+
+Priority
+--------
+
+The provided yaml file will be loaded first (if it exists), and environment
+variables will override the yaml files.
---
+++ goconfig.go
@@ -0,0 +1,181 @@
+package goconfig
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"reflect"
+	"strings"
+	"sync"
+	"syscall"
+
+	"github.com/caarlos0/env"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	DebugError   = iota
+	DebugWarning = iota
+	DebugInfo    = iota
+	DebugVerbose = iota
+)
+
+var (
+	debugLevelMap = map[string]int{
+		"error":   DebugError,
+		"warning": DebugWarning,
+		"info":    DebugInfo,
+		"verbose": DebugVerbose,
+	}
+)
+
+// Config will contain the config loaded from the yaml file.
+type Config struct {
+	Debug string `yaml:"debug" env:"DEBUG"`
+	// filename is the path and filename of the yaml config file.
+	filename  string
+	listening bool
+	// Mutex guards readwrite access to Config.
+	sync.Mutex
+}
+
+func (c *Config) GetFilename() string {
+	return c.filename
+}
+
+func (c *Config) SetFilename(filename string) {
+	c.filename = filename
+}
+
+func (c *Config) IsListening() bool {
+	return c.listening
+}
+
+func (c *Config) SetListening(listening bool) {
+	c.listening = listening
+}
+
+func (c *Config) DebugLevel(level string) bool {
+	return debugLevelMap[c.Debug] >= debugLevelMap[level]
+}
+
+type Configterface interface {
+	GetFilename() string
+	IsListening() bool
+	SetListening(bool)
+	Lock()
+	Unlock()
+}
+
+type MissingRequiredStructFields struct {
+	missing []string
+}
+
+func (e MissingRequiredStructFields) Error() string {
+	return fmt.Sprintf("The following struct fields have missing values: %s", strings.Trim(fmt.Sprintf("%v", e.missing), "[]"))
+}
+
+// Loads (or reloads) the config file from disk.
+func Load(c Configterface) error {
+	if reflect.ValueOf(c).Kind() != reflect.Ptr {
+		panic("Load only accepts pointers to structs")
+	}
+	data, err := ioutil.ReadFile(c.GetFilename())
+	c.Lock()
+	defer c.Unlock()
+	if err == nil {
+		if err := yaml.Unmarshal(data, c); err != nil {
+			return err
+		}
+	}
+	if err := env.Parse(c); err != nil {
+		return err
+	}
+	if err := findMissingRequiredFields(c); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Reloads the config file on SIGHUP.
+func ListenForSignals(c Configterface) {
+	if reflect.ValueOf(c).Kind() != reflect.Ptr {
+		panic("ListenForSignals only accepts pointers to structs")
+	}
+	c.Lock()
+	defer c.Unlock()
+	if c.IsListening() {
+		return
+	}
+	c.SetListening(true)
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGHUP)
+	go func() {
+		for {
+			<-s
+			if err := Load(c); err != nil {
+				panic(fmt.Sprintf("config file error: %s", err))
+			}
+		}
+	}()
+}
+
+func findMissingRequiredFields(val interface{}) error {
+	var missing []string
+	value := reflect.ValueOf(val)
+	for {
+		switch value.Kind() {
+		case reflect.Struct:
+			for i := 0; i < value.NumField(); i++ {
+				tag := value.Type().Field(i).Tag
+				name := value.Type().Field(i).Name
+				field := value.Field(i)
+				if tag.Get("required") == "true" && isZero(field) {
+					missing = append(missing, name)
+				}
+			}
+			if missing != nil {
+				return MissingRequiredStructFields{missing}
+			}
+			return nil
+		case reflect.Ptr:
+			if value.IsNil() {
+				return errors.New("nil pointer!")
+			}
+			value = reflect.Indirect(value)
+		default:
+			return errors.New("Not a struct!")
+		}
+	}
+}
+
+// Shamelessly stolen from the 2nd answer of https://stackoverflow.com/questions/23555241/golang-reflection-how-to-get-zero-value-of-a-field-type
+func isZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Func, reflect.Map, reflect.Slice:
+		return v.IsNil()
+	case reflect.Array:
+		z := true
+		for i := 0; i < v.Len(); i++ {
+			z = z && isZero(v.Index(i))
+		}
+		return z
+	case reflect.Struct:
+		z := true
+		for i := 0; i < v.NumField(); i++ {
+			if v.Field(i).CanSet() {
+				z = z && isZero(v.Field(i))
+			}
+		}
+		return z
+	case reflect.Ptr:
+		return v.IsNil() || isZero(reflect.Indirect(v))
+	}
+	// Compare other types directly:
+	z := reflect.Zero(v.Type())
+	result := v.Interface() == z.Interface()
+
+	return result
+}
